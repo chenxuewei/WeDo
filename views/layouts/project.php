b3b85aeab6eb90ff1da03e8df4a983f907e53af3
LYT <249822704@qq.com>
2016-07-15 19:29:02 +0800

    版本2.3

--- views/layouts/project.php
+++ views/layouts/project.php
@@ -493,9 +493,9 @@ AppAsset::register($this);
                     </li>
                     <li><a href="#"><i class="fa fa-sitemap fa-fw"><div class="icon-bg bg-dark"></div></i><span class="menu-title">公众号管理</span><span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
-                            <li><a href="#"><i class="fa fa-angle-right"></i><span class="submenu-title">添加公众号</span></a>
+                            <li><a href="?r=administration/index"><i class="fa fa-angle-right"></i><span class="submenu-title">添加公众号</span></a>
                             </li>
-                            <li><a href="#"><i class="fa fa-angle-right"></i><span class="submenu-title">查看公众号</span></a>
+                            <li><a href="?r=administration/sel"><i class="fa fa-angle-right"></i><span class="submenu-title">查看公众号</span></a>
                             </li>
                         </ul>
                     </li>   
