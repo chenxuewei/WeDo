1454f441583f9a7ef5663ce236b0f1989cebd1c2
LYT <249822704@qq.com>
2016-07-16 12:40:57 +0800

    李彦廷 安装页面修改

---
+++ views/Administration/glist.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+</head>
+
+<body class=" ">
+    <div>
+
+        <!--BEGIN BACK TO TOP--><a id="totop" href="#"><i class="fa fa-angle-up"></i></a>
+        <!--END BACK TO TOP-->
+        <!--BEGIN TOPBAR-->
+        <div id="header-topbar-option-demo" class="page-header-topbar">
+
+            <!--BEGIN MODAL CONFIG PORTLET-->
+
+            <!--END MODAL CONFIG PORTLET-->
+        </div>
+        <!--END TOPBAR-->
+        <div id="wrapper">
+            <!--BEGIN SIDEBAR MENU-->
+            <nav id="sidebar" role="navigation" class="navbar-default navbar-static-side">
+                <div class="sidebar-collapse menu-scroll">
+                    <ul id="side-menu" class="nav">
+                        <li class="user-panel">
+                            <div class="thumb"><img src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" alt="" class="img-circle" />
+                            </div>
+                            <div class="info">
+                                <p>admin</p>
+
+                            </div>
+                            <div class="clearfix"></div>
+                        </li>
+
+
+                        <!--li.charts-sum<div id="ajax-loaded-data-sidebar"></div>-->
+                    </ul>
+                </div>
+            </nav>
+            <!--END SIDEBAR MENU-->
+            <!--BEGIN CHAT FORM-->
+            <div id="chat-form" class="fixed">
+                <div class="chat-inner">
+                    <h2 class="chat-header"><a href="javascript:;" class="chat-form-close pull-right"><i class="glyphicon glyphicon-remove"></i></a><i class="fa fa-user"></i>&nbsp;
+Chat
+&nbsp;<span class="badge badge-info">3</span></h2>
+                    <div id="group-1" class="chat-group"><strong>Favorites</strong><a href="#"><span class="user-status is-online"></span><small>Verna Morton</small><span class="badge badge-info">2</span></a><a href="#"><span class="user-status is-online"></span><small>Delores Blake</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-busy"></span><small>Nathaniel Morris</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-idle"></span><small>Boyd Bridges</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-offline"></span><small>Meredith Houston</small><span class="badge badge-info is-hidden">0</span></a>
+                    </div>
+                    <div id="group-2" class="chat-group"><strong>Office</strong><a href="#"><span class="user-status is-busy"></span><small>Ann Scott</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-offline"></span><small>Sherman Stokes</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-offline"></span><small>Florence Pierce</small><span class="badge badge-info">1</span></a>
+                    </div>
+                    <div id="group-3" class="chat-group"><strong>Friends</strong><a href="#"><span class="user-status is-online"></span><small>Willard Mckenzie</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-busy"></span><small>Jenny Frazier</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-offline"></span><small>Chris Stewart</small><span class="badge badge-info is-hidden">0</span></a><a href="#"><span class="user-status is-offline"></span><small>Olivia Green</small><span class="badge badge-info is-hidden">0</span></a>
+                    </div>
+                </div>
+                <div id="chat-box" style="top:400px">
+                    <div class="chat-box-header"><a href="#" class="chat-box-close pull-right"><i class="glyphicon glyphicon-remove"></i></a><span class="user-status is-online"></span><span class="display-name">Willard Mckenzie</span><small>Online</small>
+                    </div>
+                  <div class="chat-content">
+                        <ul class="chat-box-body">
+                            <li>
+                                <p><img src="https://s3.amazonaws.com/uifaces/faces/twitter/kolage/128.jpg" class="avt" /><span class="user">John Doe</span><span class="time">09:33</span>
+                                </p>
+                                <p>Hi Swlabs, we have some comments for you.</p>
+                            </li>
+                            <li class="odd">
+                                <p><img src="https://s3.amazonaws.com/uifaces/faces/twitter/alagoon/48.jpg" class="avt" /><span class="user">Swlabs</span><span class="time">09:33</span>
+                                </p>
+                                <p>Hi, we're listening you...</p>
+                            </li>
+                        </ul>
+                    </div>
+                   <div class="chat-textarea">
+                        <input placeholder="Type your message" class="form-control" />
+                    </div>
+                </div>
+            </div>
+            <!--END CHAT FORM-->
+            <!--BEGIN PAGE WRAPPER-->
+            <div id="page-wrapper">
+                <!--BEGIN TITLE & BREADCRUMB PAGE-->
+                <div id="title-breadcrumb-option-demo" class="page-title-breadcrumb">
+                    <div class="page-header pull-left">
+                        <div class="page-title">Form Validation</div>
+                    </div>
+                    <ol class="breadcrumb page-breadcrumb">
+                        <li><i class="fa fa-home"></i>&nbsp;<a href="index.html">后台</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
+                        <li><a href="#">公众号管理</a>&nbsp;&nbsp;<i class="fa fa-angle-right"></i>&nbsp;&nbsp;</li>
+                        <li class="active">添加公众号</li>
+                    </ol>
+                    <div class="btn btn-blue reportrange hide"><i class="fa fa-calendar"></i>&nbsp;<span></span>&nbsp;report&nbsp;<i class="fa fa-angle-down"></i>
+                        <input type="hidden" name="datestart" />
+                        <input type="hidden" name="endstart" />
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+                <!--END TITLE & BREADCRUMB PAGE-->
+                <!--BEGIN CONTENT-->
+                <center>
+                <div class="page-content">
+                    <div class="row">
+
+                        <div class="col-lg-6">
+
+                            <div class="portlet box portlet-blue">
+                                <div class="portlet-header">
+                                    <div class="caption">Sign Up Form</div>
+                                    <div class="tools"><i class="fa fa-chevron-up"></i><i data-toggle="modal" data-target="#modal-config" class="fa fa-cog"></i><i class="fa fa-refresh"></i><i class="fa fa-times"></i>
+                                    </div>
+                                </div>
+                                <div class="portlet-body">
+                                    <form action="?r=administration/add" method="post" class="form-validate-signup">
+                                   
+                                <table width="200" height="300">
+                                <input name="_csrf" type="hidden" id="_csrf" value="<?= Yii::$app->request->csrfToken ?>" />
+                                    <tr>
+                                        <td>公众号名称:</td>
+                                        <td>
+                                            <input type="text" name="aname" />
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>Appid:</td>
+                                        <td>
+                                            <input type="text" name="appid" /> 
+                                         </td>
+                                </tr>
+                                <tr>
+                                    <td>Appsecret：</td>
+                                    <td>
+                                        <input type="text" name="appsecret" />
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>内容：</td>
+                                    <td>
+                                        <textarea name="account"></textarea>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    
+                                    <td colspan="2">
+                                        <input type="submit" class="btn btn-success btn-block" value="提交" />
+                                        <input type="reset" class="btn btn-success btn-block" value="返回" />
+
+                                    </td>
+                                </tr>
+                                <?php 
+                                    if(!empty($error)){
+                                        echo $error['aname'][0];
+                                        echo $error['appid'][0];
+                                        echo $error['appsecret'][0];
+                                        echo $error['account'][0];
+                                    }
+                                 ?>
+
+                                
+                            </table>
+                                       
+
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                </center> 
+                <!--END CONTENT-->
+            </div>
+            <!--BEGIN FOOTER-->
+            <div id="footer">
+
+            </div>
+            <!--END FOOTER-->
+            <!--END PAGE WRAPPER-->
+        </div>
+    </div>
+    <script src="js/jquery-1.10.2.min.js"></script>
+    <script src="js/jquery-migrate-1.2.1.min.js"></script>
+    <script src="js/jquery-ui.js"></script>
+    <!--loading bootstrap js-->
+    <script src="vendors/bootstrap/js/bootstrap.min.js"></script>
+    <script src="vendors/bootstrap-hover-dropdown/bootstrap-hover-dropdown.js"></script>
+    <script src="js/html5shiv.js"></script>
+    <script src="js/respond.min.js"></script>
+    <script src="vendors/metisMenu/jquery.metisMenu.js"></script>
+    <script src="vendors/slimScroll/jquery.slimscroll.js"></script>
+    <script src="vendors/jquery-cookie/jquery.cookie.js"></script>
+    <script src="vendors/iCheck/icheck.min.js"></script>
+    <script src="vendors/iCheck/custom.min.js"></script>
+    <script src="vendors/jquery-notific8/jquery.notific8.min.js"></script>
+    <script src="vendors/jquery-highcharts/highcharts.js"></script>
+    <script src="js/jquery.menu.js"></script>
+    <script src="vendors/jquery-pace/pace.min.js"></script>
+    <script src="vendors/holder/holder.js"></script>
+    <script src="vendors/responsive-tabs/responsive-tabs.js"></script>
+    <script src="vendors/jquery-news-ticker/jquery.newsTicker.min.js"></script>
+    <script src="vendors/moment/moment.js"></script>
+    <script src="vendors/bootstrap-datepicker/js/bootstrap-datepicker.js"></script>
+    <script src="vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+    <!--CORE JAVASCRIPT-->
+    <script src="js/main.js"></script>
+    <!--LOADING SCRIPTS FOR PAGE-->
+    <script src="vendors/jquery-validate/jquery.validate.min.js"></script>
+    <script src="js/form-validation.js"></script>
+    <script type="text/javascript">
+        (function(i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function() {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+        ga('create', 'UA-145464-14', 'auto');
+        ga('send', 'pageview');
+    </script>
+</body>
+
+</html>
