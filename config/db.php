52416993c473941291285abc43908655e5fe316a
hujian <973584073@qq.com>
2016-07-18 16:05:15 +0800

    胡健提交

--- config/db.php
+++ config/db.php
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
 <?php
                 return [
                     'class' => 'yii\db\Connection',
                     'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=wedo',
                     'username' => 'root',
-                    'password' => 'root',
+                    'password' => '123',
                     'charset' => 'utf8',
                     'tablePrefix' => 'wd_',   //加入前缀名称
                 ];
-=======
-<?php 
-                    return [
-                        'class' => 'yii\db\Connection',
-                        'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=wedo',
-                        'username' => 'root',
-                        'password' => '123',
-                        'charset' => 'utf8',
-                        'tablePrefix' => 'wd_',   //加入前缀名称
-                    ];
-
->>>>>>> 05caaaa97def68f8aa660b057722de8c86aa7f85
