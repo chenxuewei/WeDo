7171c8bb289caffddcf857ab3e0215d90fd30189
Your Name <you@example.com>
2016-07-18 15:41:50 +0800

    yfc

---
+++ models/Account.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+
+class Account extends ActiveRecord
+{
+
+	/**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'wd_account';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['mid', 'uid'], 'integer'],
+            [['aname', 'account', 'appid', 'appsecret'], 'required'],
+            [['aname', 'account', 'atok', 'aurl'], 'string', 'max' => 255],
+            [['appid', 'appsecret', 'atoken'], 'string', 'max' => 50],
+            [['aname'],'unique'],
+            [['appid'], 'unique'],
+            [['appsecret'], 'unique']
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'aid' => 'Aid',
+            'mid' => 'Mid',
+            'uid' => 'Uid',
+            'aname' => 'Aname',
+            'account' => 'Account',
+            'appid' => 'Appid',
+            'appsecret' => 'Appsecret',
+            'atoken' => 'Atoken',
+            'atok' => 'Atok',
+            'aurl' => 'Aurl',
+        ];
+    }
+}
